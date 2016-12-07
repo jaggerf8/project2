30953574202e26b5726e78e11e2f50d95fc71272
Jagger S Ferreira <jsf22@afsconnect1.njit.edu>
2016-12-07 04:08:36 -0500

    curl functions

---
+++ curls.php
@@ -0,0 +1,69 @@
+<?php
+
+
+class curls
+{
+//echo " we are in curls file";
+public function post($url,$content)
+{
+	
+	$curl = curl_init();
+	
+	curl_setopt_array($curl, array(
+    	CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $url,
+	CURLOPT_USERAGENT => 'curl get',
+	CURLOPT_POST => 1,
+	CURLOPT_POSTFIELDS => $content));
+	
+	$info= curl_exec($curl);
+	if(curl_errno($curl)){
+	        throw new Exception(curl_error($curl));}
+	return  $info;
+	
+	curl_close($curl);
+
+}
+public function get($url)
+{
+echo " we are in get function in curls file";
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_URL => $url,
+	CURLOPT_USERAGENT => 'curl get'));
+
+	$info = curl_exec($curl);
+	if(curl_errno($curl)){
+	        throw new Exception(curl_error($curl));}
+	
+	return $info;
+	curl_close($curl);
+}							
+
+public function get2($url)
+{
+echo " test";
+$curl = curl_init();
+
+         curl_setopt_array($curl, array(
+	         CURLOPT_RETURNTRANSFER => 1,
+		         CURLOPT_URL => $url,
+			         CURLOPT_USERAGENT => 'curl get'));
+
+				         $info = curl_exec($curl);
+					         if(curl_errno($curl)){
+						                 throw new
+								 Exception(curl_error($curl));}
+								 echo $info;
+								         return
+									 $info;
+									         curl_close($curl);
+
+
+}
+
+}
+
+?>
